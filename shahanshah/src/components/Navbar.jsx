51b4ef70195ecdb6dd78c1d2f68b7081f86e0a3b
Ayush Trivedi <ayush.trivedi0511@gmail.com>
2025-08-23 01:43:22 +0530

    header-and-footer-added

---
+++ shahanshah/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+function Navbar() {
+  return (
+    <>
+    <nav className='bg-amber-200 text-black h-7.5'>
+        <div className="text-2xl text-center">Shahanshahi Mission</div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
